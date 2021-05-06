5c2a89e5509fb6cb1b655a0e9108bbe7b7f6493c
Alex Michalak <atmichalak@gmail.com>
2021-05-06 19:56:06 -0400

    complete chapter 6 -- add image5.ppm

---
+++ rtweekend.hpp
@@ -0,0 +1,30 @@
+//
+// Created by atm on 5/6/21.
+//
+
+#ifndef RTIOW_RTWEEKEND_HPP
+#define RTIOW_RTWEEKEND_HPP
+
+#include <cmath>
+#include <limits>
+#include <memory>
+
+// Usings
+using std::shared_ptr;
+using std::make_shared;
+using std::sqrt;
+
+// Constants
+const double infinity = std::numeric_limits<double>::infinity();
+const double pi = 3.1415926535897932385;
+
+// Utility Functions
+inline double degrees_to_radians(double degrees) {
+    return degrees * pi / 180.0;
+}
+
+// Common Headers
+#include "ray.hpp"
+#include "vec3.hpp"
+
+#endif //RTIOW_RTWEEKEND_HPP
